26bee382ae8cfd5395b6100e819330eab5356b55
Darren O'Neale <doneale3@gmail.com>
2017-01-21 18:45:36 -0500

    Reslolving merge

---
+++ Attraction/Assets/scripts/util/Constants.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Types;
+
+namespace Util {
+
+	/// Holds all constant variables
+	public class CONST {
+		// ------------------SCENES------------------
+
+		public const string SCENE_SPLASH = "splash";
+		public const string SCENE_MENU = "menu";
+		public const string SCENE_LEVELS = "levels";
+		public const string SCENE_PLAY = "test";
+
+		// ------------------DATA------------------
+		
+		
+	}
+}
