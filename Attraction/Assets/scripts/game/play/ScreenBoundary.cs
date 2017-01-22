2c54dd7a5e719c341aab45290b074622124665de
Darren O'Neale <doneale3@gmail.com>
2017-01-22 12:20:15 -0500

    fixed gravity well death

--- Attraction/Assets/scripts/game/play/ScreenBoundary.cs
+++ Attraction/Assets/scripts/game/play/ScreenBoundary.cs
@@ -6,14 +6,19 @@ public class ScreenBoundary : MonoBehaviour {
 	public ExplosionParticles explosion;
 	public GameObject playerRef;
 
+	ShipController ship;
+
 	void Start()
 	{
 		playerRef = GameObject.FindGameObjectWithTag("Player");
+		ship = playerRef.GetComponent<ShipController>();
 	}
 
 	void OnTriggerExit2D(Collider2D col)
 	{
 		if (col.gameObject.tag.Equals("Player")) {
+			if (ship.dying || ship.state == ShipController.State.RESET)
+				return;
 			explosion.gameObject.transform.position = col.gameObject.transform.position;
 			explosion.SpawnParticles();
 			playerRef.GetComponent<ShipController>().ReduceLives();
