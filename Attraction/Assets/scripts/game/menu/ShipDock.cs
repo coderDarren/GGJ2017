8a28602dcd44aa38b560e8a1329f1a98c5cab54e
Darren O'Neale <doneale3@gmail.com>
2017-02-06 02:27:01 -0500

    Ship Selection Page

---
+++ Attraction/Assets/scripts/game/menu/ShipDock.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Types;
+
+public class ShipDock : MonoBehaviour {
+
+	public float rotationSpeed = 5;
+
+	RectTransform rect;
+
+	void Start() {
+		rect = GetComponent<RectTransform>();
+	}
+
+	IEnumerator RotateShipDock(float deg) {
+		float dist = Mathf.Abs(rect.eulerAngles.z - deg);
+		while (dist > 0.1f) {
+			rect.rotation = Quaternion.Lerp(rect.rotation, Quaternion.Euler(0,0,deg), rotationSpeed * Time.deltaTime);
+			dist = Mathf.Abs(rect.eulerAngles.z - deg);
+			yield return null;
+		}
+		rect.rotation = Quaternion.Euler(0,0,deg);
+	}
+
+	public void SelectShip(ShipType ship) {
+		StopCoroutine("RotateShipDock");
+		switch (ship) {
+			case ShipType.SHIP_01: StartCoroutine("RotateShipDock", 0); break;
+			case ShipType.SHIP_02: StartCoroutine("RotateShipDock", 45); break;
+			case ShipType.SHIP_03: StartCoroutine("RotateShipDock", 90); break;
+			case ShipType.SHIP_04: StartCoroutine("RotateShipDock", 135); break;
+			case ShipType.SHIP_05: StartCoroutine("RotateShipDock", 180); break;
+			case ShipType.SHIP_06: StartCoroutine("RotateShipDock", -135); break;
+			case ShipType.SHIP_07: StartCoroutine("RotateShipDock", -90); break;
+			case ShipType.SHIP_08: StartCoroutine("RotateShipDock", -45); break;
+		}
+	}
+}
---
+++ Attraction/Assets/scripts/game/menu/ShipSelectEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Menu;
+using Types;
+
+public class ShipSelectEvent : ButtonEvent {
+
+	public ShipType shipType;
+
+	ShipDock dock;
+
+	void Start() {
+		dock = GameObject.FindObjectOfType<ShipDock>();
+	}
+
+	public override void OnItemUp() {
+		dock.SelectShip(shipType);
+	}
+}
